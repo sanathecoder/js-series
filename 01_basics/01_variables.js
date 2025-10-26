016608619f2d155e6a56cb7952c31bc8e2bcb8ba
sanathecoder <sanasana33377737@gmail.com>
2025-10-26 15:47:49 +0500

    variable

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 121212
+let accountEmail = "sana@google.com"
+var accountpassword = "12345"
+accountCity = "Multan"
+
+
+//  accountId = 2 not allowed
+
+accountEmail = "hr@gmail.com"
+accountpassword = "454545"
+accountCity = "lahore"
+
+console.log(accountId)
+console.table([accountId,accountEmail,accountpassword,accountCity])
