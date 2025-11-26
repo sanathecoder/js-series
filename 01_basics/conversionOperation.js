c893db6eb54bf1ecf3c176df75bdfa153f899d69
sanathecoder <sanasana33377737@gmail.com>
2025-11-26 16:16:28 +0500

    Conversion of data types

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,17 @@
+// let score = "33abc"
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+
+let isLoggedIn = 0
+let booleanisLoggedIn = Boolean(isLoggedIn)
+console.log(booleanisLoggedIn)
+
+// 1 => true    0 => false
+// string => true
+
+let someNumb = 33
+let stringNum = String(someNumb)
+console.log(stringNum)
+console.log(typeof stringNum)
