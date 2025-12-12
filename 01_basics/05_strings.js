00bee156829bbe5fef8a36728bf457edd88aef62
sanathecoder <sanasana33377737@gmail.com>
2025-12-12 07:40:06 +0500

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,19 @@
+// const name = "Sana"
+// const RepoCount = 30
+
+// console.log(`My name is ${name} and my repoCount is ${RepoCount} `)
+
+const gameName = new String('SanaKhan')
+// console.log(gameName.charAt(2))
+// console.log(gameName.indexOf('n'))
+// const newString = gameName.substring(1,4)
+// console.log(newString)
+// const anotherString = gameName.slice(-7,4)
+// console.log(anotherString)
+// const newStringOne = "    Sana    "
+// console.log(newStringOne)
+// console.log(newStringOne.trim())
+const url = "https:/sana.com/sana20%khan"
+// const changeURL = url.replace("20%", '-')
+// console.log(changeURL)
+console.log(url.split("/"))
