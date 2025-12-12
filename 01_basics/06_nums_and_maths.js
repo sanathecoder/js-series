b0852d7c043ed871e74763bac520e075036a6928
sanathecoder <sanasana33377737@gmail.com>
2025-12-12 08:31:06 +0500

    num and maths

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,24 @@
+// const score = 20
+// const balance = new Number(100)
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(2))
+// const otherNum = 123.839
+// console.log(otherNum.toPrecision(3))
+// const hundreds = 100000000
+// console.log(hundreds.toLocaleString())
+
+
+
+//   Maths
+
+console.log(Math)
+// console.log(Math.round(4.8))
+// console.log(Math.ceil(4.8))
+// console.log(Math.floor(4.8))
+// console.log(Math.min(4 , 3 , 6))
+// console.log(Math.max(4 , 3 , 6))
+// console.log(Math.random())
+// console.log(Math.floor((Math.random()*10 + 1)))
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
