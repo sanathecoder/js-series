5b6704bb38143c66922447c83aacaad5033760fd
sanathecoder <sanasana33377737@gmail.com>
2025-12-13 07:45:49 +0500

    arrays

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,17 @@
+let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toLocaleString())
+// console.log(myDate.toDateString())
+// console.log(myDate.toTimeString())
+// console.log(typeof myDate)
+
+// let createdDate = new Date(2025,0,23)
+let createdDate = new Date("10-23-2025")
+// console.log(createdDate.toLocaleString())
+
+let myTimeStamp = Date.now()
+// console.log(myTimeStamp)
+// console.log(createdDate.getTime())
+// console.log(Math.floor(Date.now()/1000))
+let newDate = new Date()
+console.log(newDate.getMonth() + 1)
